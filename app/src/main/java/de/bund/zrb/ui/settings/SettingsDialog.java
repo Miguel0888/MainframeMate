1357ec119653a90b465dba6ae412dee4e8a86f25
Miguel Angelo Fernández Iglesias <angelo.iglesias@gmx.de>
2026-02-20 04:07:34 +0100

    fix(SettingsDialog): remove unnecessary reset of clearPasswordRequested flag

--- app/src/main/java/de/bund/zrb/ui/settings/SettingsDialog.java
+++ app/src/main/java/de/bund/zrb/ui/settings/SettingsDialog.java
@@ -182,7 +182,6 @@ public class SettingsDialog {
         }
 
         settings = SettingsHelper.load();
-        clearPasswordRequested = false;
 
         createGeneralContent(generalContent, parent);
         createTransformContent(transformContent);
