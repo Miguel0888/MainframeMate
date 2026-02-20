d06d36ef25569cc6640723e6a2e0b73895bfbccb
Miguel Angelo Fernández Iglesias <angelo.iglesias@gmx.de>
2026-02-20 23:52:44 +0100

    feat: add initial project structure with build configuration and auxiliary classes

---
+++ ndv/src/main/java/com/softwareag/naturalone/natural/auxiliary/IInsertLabels.java
@@ -0,0 +1,9 @@
+package com.softwareag.naturalone.natural.auxiliary;
+
+public interface IInsertLabels {
+    boolean isInsertLabels();
+
+    String getLabelFormat();
+
+    boolean isCreateNewLine();
+}
