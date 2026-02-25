4e6dfc48eb63cb6d2ecdae22dfdf3c2ca6614e57
Miguel Angelo Fernández Iglesias <angelo.iglesias@gmx.de>
2026-02-25 13:38:20 +0100

    add app icon to about menu

--- app/src/main/java/de/bund/zrb/ui/commands/ShowAboutDialogMenuCommand.java
+++ app/src/main/java/de/bund/zrb/ui/commands/ShowAboutDialogMenuCommand.java
@@ -1,8 +1,10 @@
 package de.bund.zrb.ui.commands;
 
+import de.bund.zrb.ui.branding.IconThemeInstaller;
 import de.zrb.bund.api.ShortcutMenuCommand;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class ShowAboutDialogMenuCommand extends ShortcutMenuCommand {
 
@@ -24,8 +26,22 @@ public class ShowAboutDialogMenuCommand extends ShortcutMenuCommand {
 
     @Override
     public void perform() {
+        // Use the largest available app logo instead of the default info icon
+        ImageIcon logoIcon = null;
+        Image appIcon = IconThemeInstaller.getAppIcon(1024);
+        if (appIcon == null) {
+            appIcon = IconThemeInstaller.getAppIcon(256);
+        }
+        if (appIcon != null) {
+            // Scale to a nice display size for the dialog (128px)
+            Image scaled = appIcon.getScaledInstance(128, 128, Image.SCALE_SMOOTH);
+            logoIcon = new ImageIcon(scaled);
+        }
+
         JOptionPane.showMessageDialog(parent,
                 "MainframeMate\nVersion 5.3.0\n© 2026 GZD",
-                "Über", JOptionPane.INFORMATION_MESSAGE);
+                "Über MainframeMate",
+                logoIcon != null ? JOptionPane.PLAIN_MESSAGE : JOptionPane.INFORMATION_MESSAGE,
+                logoIcon);
     }
 }
