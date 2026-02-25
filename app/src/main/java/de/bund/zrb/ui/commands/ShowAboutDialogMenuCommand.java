5b1451826f27d8f3f2f49c890a78cc0c9a2d83dd
Miguel Angelo Fernández Iglesias <angelo.iglesias@gmx.de>
2026-02-25 13:47:42 +0100

    feat(about-dialog): enhance About dialog with app name, version, copyright, and MIT License

--- app/src/main/java/de/bund/zrb/ui/commands/ShowAboutDialogMenuCommand.java
+++ app/src/main/java/de/bund/zrb/ui/commands/ShowAboutDialogMenuCommand.java
@@ -39,10 +39,77 @@ public class ShowAboutDialogMenuCommand extends ShortcutMenuCommand {
             logoIcon = new ImageIcon(scaled);
         }
 
+        // --- Build the about panel ---
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        // Logo
+        if (logoIcon != null) {
+            JLabel logoLabel = new JLabel(logoIcon);
+            logoLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+            panel.add(logoLabel);
+            panel.add(Box.createVerticalStrut(12));
+        }
+
+        // App name + version + copyright
+        JLabel nameLabel = new JLabel("MainframeMate");
+        nameLabel.setFont(nameLabel.getFont().deriveFont(Font.BOLD, 18f));
+        nameLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel.add(nameLabel);
+
+        JLabel versionLabel = new JLabel("Version " + BuildInfo.getVersion());
+        versionLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel.add(versionLabel);
+
+        JLabel copyrightLabel = new JLabel("© 2026 GZD");
+        copyrightLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel.add(copyrightLabel);
+
+        panel.add(Box.createVerticalStrut(16));
+
+        // Separator
+        JSeparator sep = new JSeparator();
+        sep.setMaximumSize(new Dimension(Integer.MAX_VALUE, 2));
+        panel.add(sep);
+        panel.add(Box.createVerticalStrut(8));
+
+        // MIT License text
+        String licenseText =
+                "MIT License\n\n" +
+                "Copyright (c) 2026 GZD\n\n" +
+                "Permission is hereby granted, free of charge, to any person obtaining a copy\n" +
+                "of this software and associated documentation files (the \"Software\"), to deal\n" +
+                "in the Software without restriction, including without limitation the rights\n" +
+                "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n" +
+                "copies of the Software, and to permit persons to whom the Software is\n" +
+                "furnished to do so, subject to the following conditions:\n\n" +
+                "The above copyright notice and this permission notice shall be included in all\n" +
+                "copies or substantial portions of the Software.\n\n" +
+                "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n" +
+                "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n" +
+                "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n" +
+                "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n" +
+                "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n" +
+                "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n" +
+                "SOFTWARE.";
+
+        JTextArea licenseArea = new JTextArea(licenseText);
+        licenseArea.setEditable(false);
+        licenseArea.setLineWrap(true);
+        licenseArea.setWrapStyleWord(true);
+        licenseArea.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 11));
+        licenseArea.setBackground(panel.getBackground());
+        licenseArea.setBorder(BorderFactory.createEmptyBorder(4, 8, 4, 8));
+
+        JScrollPane scrollPane = new JScrollPane(licenseArea);
+        scrollPane.setPreferredSize(new Dimension(520, 200));
+        scrollPane.setBorder(BorderFactory.createTitledBorder("Lizenz"));
+        scrollPane.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel.add(scrollPane);
+
         JOptionPane.showMessageDialog(parent,
-                "MainframeMate\nVersion " + BuildInfo.getVersion() + "\n© 2026 GZD",
+                panel,
                 "Über MainframeMate",
-                logoIcon != null ? JOptionPane.PLAIN_MESSAGE : JOptionPane.INFORMATION_MESSAGE,
-                logoIcon);
+                JOptionPane.PLAIN_MESSAGE);
     }
 }
