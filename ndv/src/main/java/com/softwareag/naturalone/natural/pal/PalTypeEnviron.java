32ef1476d3cc5073730c8a8f6c176832d23e8f5c
Miguel Angelo Fernández Iglesias <angelo.iglesias@gmx.de>
2026-03-02 00:28:02 +0100

    feat(pal): refactor PalTypeEnviron for improved OS handling and state management

--- ndv/src/main/java/com/softwareag/naturalone/natural/pal/PalTypeEnviron.java
+++ ndv/src/main/java/com/softwareag/naturalone/natural/pal/PalTypeEnviron.java
@@ -2,67 +2,92 @@ package com.softwareag.naturalone.natural.pal;
 
 import com.softwareag.naturalone.natural.pal.external.EAttachSessionType;
 import com.softwareag.naturalone.natural.pal.external.IPalTypeEnviron;
+import com.softwareag.naturalone.natural.pal.external.PalTrace;
 
-public class PalTypeEnviron extends PalType implements IPalTypeEnviron {
+import java.io.IOException;
+
+public final class PalTypeEnviron extends PalType implements IPalTypeEnviron {
     private static final long serialVersionUID = 1L;
     private static final int PAL_VERSION = 47;
 
     private int ndvClientVersion;
     private int natVersion;
     private int palVersion;
-    private String operatingSystem = "";
+    private String opSys = "";
     private String sessionId = "";
-    private int osVersion;
+    private int opsysVer;
     private String startupCommands = "";
     private int ndvVersion;
-    private int terminalModel;
+    private int tModel;
     private int logonCounter;
-    private int featureFlags;
+    private int flags;
     private int webVersion;
-    private int ndvTypeField;
+    private int ndvType;
 
-    public PalTypeEnviron() { super(); type = 0; }
+    public PalTypeEnviron() {
+    }
+
     public PalTypeEnviron(int logonCounter) {
-        this();
+        super.type = 0;
+        this.natVersion = 8380;
+        this.opSys = System.getProperty("os.name");
+        String[] verParts = System.getProperty("os.version", "0").split("\\.");
+        if (verParts != null) {
+            String combined = "";
+            for (int i = 0; i < verParts.length; ++i) {
+                try {
+                    Integer.valueOf(verParts[i]);
+                    combined = combined + verParts[i];
+                } catch (NumberFormatException e) {
+                    try {
+                        PalTrace.text("os.version problem:" + verParts[i]);
+                    } catch (IOException ignored) {
+                    }
+                }
+            }
+            if (!combined.isEmpty()) {
+                this.opsysVer = Integer.valueOf(combined);
+            }
+        }
         this.logonCounter = logonCounter;
-        this.natVersion = 0;
-        this.palVersion = PAL_VERSION;
-        String os = System.getProperty("os.name", "").toLowerCase();
-        if (os.contains("windows")) { this.operatingSystem = "PC"; this.osVersion = WINDOWS; }
-        else if (os.contains("linux") || os.contains("unix") || os.contains("aix") || os.contains("hp-ux") || os.contains("sunos")) {
-            this.operatingSystem = "UNIX"; this.osVersion = UNIX;
-        } else { this.operatingSystem = "PC"; this.osVersion = WINDOWS; }
     }
 
     public void serialize() {
-        intToBuffer(ndvClientVersion); intToBuffer(natVersion); intToBuffer(palVersion);
-        stringToBuffer(operatingSystem); stringToBuffer(sessionId);
-        intToBuffer(osVersion); stringToBuffer(startupCommands);
-        intToBuffer(ndvVersion); intToBuffer(terminalModel); intToBuffer(logonCounter);
-        intToBuffer(featureFlags); intToBuffer(webVersion);
+        intToBuffer(natVersion);
+        intToBuffer(PAL_VERSION);
+        stringToBuffer(opSys);
+        stringToBuffer(sessionId);
+        intToBuffer(opsysVer);
+        stringToBuffer(startupCommands);
+        intToBuffer(ndvVersion);
+        intToBuffer(tModel);
+        intToBuffer(logonCounter);
+        intToBuffer(flags);
+        intToBuffer(webVersion);
+        intToBuffer(ndvClientVersion);
     }
 
     public void restore() {
         natVersion = intFromBuffer();
         palVersion = intFromBuffer();
-        operatingSystem = stringFromBuffer();
-        sessionId = stringFromBuffer();
-        osVersion = intFromBuffer();
-        // derive ndvType from OS
-        if (operatingSystem != null) {
-            String os = operatingSystem.toUpperCase();
-            if (os.contains("MVS") || os.contains("OS/390") || os.contains("Z/OS") || os.contains("MAINFRAME")) ndvTypeField = MAINFRAME;
-            else if (os.contains("UNIX") || os.contains("LINUX") || os.contains("AIX") || os.contains("HP-UX") || os.contains("SUNOS")) ndvTypeField = UNIX;
-            else if (os.contains("PC") || os.contains("WINDOWS")) ndvTypeField = WINDOWS;
-            else if (os.contains("VMS")) ndvTypeField = VMS;
-            else ndvTypeField = MAINFRAME;
+        opSys = stringFromBuffer();
+        ndvType = 1;
+        if (opSys.compareTo("OS/390") == 0) {
+            ndvType = 1;
+        } else if (opSys.compareTo("UNIX") == 0) {
+            ndvType = 2;
+        } else if (opSys.compareTo("PC") == 0) {
+            ndvType = 3;
+        } else if (opSys.compareTo("VMS") == 0) {
+            ndvType = 4;
         }
-        // optional fields
+        sessionId = stringFromBuffer();
+        opsysVer = intFromBuffer();
         if (recordTail < recordLength) startupCommands = stringFromBuffer();
         if (recordTail < recordLength) ndvVersion = intFromBuffer();
-        if (recordTail < recordLength) terminalModel = intFromBuffer();
+        if (recordTail < recordLength) tModel = intFromBuffer();
         if (recordTail < recordLength) logonCounter = intFromBuffer();
-        if (recordTail < recordLength) featureFlags = intFromBuffer();
+        if (recordTail < recordLength) flags = intFromBuffer();
         if (recordTail < recordLength) webVersion = intFromBuffer();
     }
 
@@ -72,24 +97,47 @@ public class PalTypeEnviron extends PalType implements IPalTypeEnviron {
     public int getNatVersion() { return natVersion; }
     public int getNdvVersion() { return ndvVersion; }
     public int getPalVersion() { return palVersion; }
-    public int getNdvType() { return ndvTypeField; }
+    public int getNdvType() { return ndvType; }
     public int getWebVersion() { return webVersion; }
     public void setWebVersion(int v) { this.webVersion = v; }
 
-    public boolean isMfUnicodeSrcPossible() { return (featureFlags & 8) != 0; }
-    public boolean isWebIOServer() { return (featureFlags & 4) != 0; }
-    public boolean performsTimeStampChecks() { return (featureFlags & 256) != 0; }
-    public void setRichGui(boolean e) { featureFlags = e ? featureFlags | 16 : featureFlags & ~16; }
-    public void setNdvClientClientId(int id) { /* stored in flags */ }
-    public void setNdvClientClientVersion(int v) { ndvClientVersion = v; }
-    public void setWebBrowserIO(boolean e) { featureFlags = e ? featureFlags | 4 : featureFlags & ~4; }
-    public void setNfnPrivateMode(boolean e) { featureFlags = e ? featureFlags | 64 : featureFlags & ~64; }
-    public void setTimeStampChecks(boolean e) { featureFlags = e ? featureFlags | 256 : featureFlags & ~256; }
+    public boolean isMfUnicodeSrcPossible() { return (flags & 8) == 8; }
+    public boolean isWebIOServer() { return (flags & 4) == 4; }
+    public boolean performsTimeStampChecks() { return (flags & 256) == 256; }
+
+    public void setRichGui(boolean e) {
+        if (e) { flags |= 16; }
+    }
+
+    public void setNdvClientClientId(int id) {
+        flags |= id;
+    }
+
+    public void setNdvClientClientVersion(int v) {
+        ndvClientVersion = v;
+    }
+
+    public void setWebBrowserIO(boolean e) {
+        if (e) { flags |= 4; }
+    }
+
+    public void setNfnPrivateMode(boolean e) {
+        if (e) { flags |= 64; }
+    }
+
+    public void setTimeStampChecks(boolean e) {
+        if (e) { flags |= 256; }
+    }
 
     public EAttachSessionType getAttachSessionType() {
-        if ((featureFlags & 512) != 0) return EAttachSessionType.RPC;
-        if ((featureFlags & 1024) != 0) return EAttachSessionType.NAT;
-        if ((featureFlags & 2048) != 0) return EAttachSessionType.NJX;
-        return EAttachSessionType.NDV;
+        EAttachSessionType result = EAttachSessionType.NDV;
+        if ((flags & 2048) == 2048) {
+            result = EAttachSessionType.NJX;
+        } else if ((flags & 512) == 512) {
+            result = EAttachSessionType.RPC;
+        } else if ((flags & 1024) == 1024) {
+            result = EAttachSessionType.NAT;
+        }
+        return result;
     }
 }
