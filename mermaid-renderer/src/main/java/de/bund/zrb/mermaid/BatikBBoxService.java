f1016690e8c3d0cb936fa0fc39d24463ed997a7a
Miguel Angelo Fernández Iglesias <angelo.iglesias@gmx.de>
2026-03-23 03:53:04 +0100

    feat(batik): implement accurate SVG bounding box (bbox) computation using Apache Batik's GVT tree

--- mermaid-renderer/src/main/java/de/bund/zrb/mermaid/BatikBBoxService.java
+++ mermaid-renderer/src/main/java/de/bund/zrb/mermaid/BatikBBoxService.java
@@ -5,6 +5,7 @@ import org.apache.batik.bridge.GVTBuilder;
 import org.apache.batik.bridge.UserAgentAdapter;
 import org.apache.batik.gvt.GraphicsNode;
 
+import org.w3c.dom.DOMImplementation;
 import org.w3c.dom.Document;
 
 import java.awt.geom.Rectangle2D;
